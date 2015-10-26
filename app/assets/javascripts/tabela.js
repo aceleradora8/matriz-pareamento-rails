b6e466390e6a91dded0c87630d86352312644ae0
amandasv <amandasv.pm@gmail.com>
2015-10-26 14:44:35 -0200

    #<Amanda, Lucas Veloso, Raphael> Alterando

--- app/assets/javascripts/tabela.js
+++ app/assets/javascripts/tabela.js
@@ -1,14 +1,38 @@
 var val = [];
+var checkboxMarcados = [];
+
 
 function manter_historia() {
-    var checkbox = $('input[type=checkbox]:checked');
-    if(checkbox.length > 0){
+    var checkbox = $('.cbmanter:checked');
+    if(checkbox.length > 0) {
         checkbox.each(function(){
             val.push($(this).val());
-        });
 
-}
+        });
+    }
         var link = "/tabela/" + val;
         window.location.replace(link);
 
+
 }
+
+
+function associaEventoCheckbox(){
+  var checkbox = $('.cbmanter');
+    checkbox.on("click",function() {
+      if($(this).is(":checked")){
+        checkboxMarcados.push($(this));
+
+        if (checkboxMarcados.length > (checkbox.length/2)){
+          console.log(checkboxMarcados)
+        checkboxMarcados[0].prop('checked', false);
+        checkboxMarcados.splice(checkboxMarcados.indexOf(checkboxMarcados[0]),1);
+      }
+        console.log($(this))
+    }
+  });
+};
+
+$(document).ready(function() {
+  associaEventoCheckbox();
+});
