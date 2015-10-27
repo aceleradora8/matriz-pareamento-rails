ccc0bc8d67fbd92267b8b48ab6eb117fdce3c756
lucassveloso <lucas.s.veloso@hotmail.com>
2015-10-27 16:10:06 -0200

    #<Amanda,Lucas Veloso> - Configurando UTC do timezone para horario local brasileiro

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module PareamentoRails
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.default_locale = "pt-BR"
+    config.time_zone = 'Brasilia'
   end
 end
