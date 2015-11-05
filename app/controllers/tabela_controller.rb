321ab9347564b8c079f2770c0d1abc9e525ece02
lucassveloso <lucas.s.veloso@hotmail.com>
2015-11-05 10:39:51 -0200

    #<@lucassveloso, @gabrielpozza, @raphaelbraun> - Inclusão da lógica de manter aluno em histórias

--- app/controllers/tabela_controller.rb
+++ app/controllers/tabela_controller.rb
@@ -19,39 +19,67 @@ end
       @selecionados =  params[:manter].split(",").map {|i| i.to_i}
       @selecionados.each { |id| alunos_manter_historia.push(Aluno.find(id)) }
     end
+
     @todas_duplas_ordenada = @todas_duplas.sort { |x,y| x.num_pareamento <=> y.num_pareamento }
-    num_duplas = @alunos.length/2
+
+    unless(alunos_manter_historia.empty?)
+      deletar_duplas_manter_historia(alunos_manter_historia)
+    end
+
+    @num_duplas = @alunos.length/2+1
     i = 0
-    while(num_duplas>0) do
-      unless(dupla_contem_alunos_juntos_do_manter_historia?(@todas_duplas_ordenada[i], alunos_manter_historia))
+
+    while(@num_duplas>0) do
         unless(lista_contem_alunos_dessa_dupla?(@duplas,@todas_duplas_ordenada[i]))
           @duplas.push(@todas_duplas_ordenada[i])
-          num_duplas-=1
+          @num_duplas-=1
         end
+        i+=1
       end
-      i+=1
-    end
+      @duplas = @duplas.compact
   end
 
   def lista_contem_alunos_dessa_dupla?(lista,dupla)
     lista.each do |dupla_lista|
-      if( dupla_lista.aluno1.id == dupla.aluno1.id ||
-          dupla_lista.aluno1.id == dupla.aluno2.id ||
-          dupla_lista.aluno2.id == dupla.aluno1.id ||
-          dupla_lista.aluno2.id == dupla.aluno2.id)
-          return true
-       end
+      if(dupla != nil && dupla_lista != nil)
+        if( dupla_lista.aluno1.id == dupla.aluno1.id ||
+            dupla_lista.aluno1.id == dupla.aluno2.id ||
+            dupla_lista.aluno2.id == dupla.aluno1.id ||
+            dupla_lista.aluno2.id == dupla.aluno2.id)
+
+            return true
+         end
+     end
     end
      false
   end
 
-   def dupla_contem_alunos_juntos_do_manter_historia?(dupla,alunos_manter_historia)
-    if(alunos_manter_historia.include?(dupla.aluno1) && alunos_manter_historia.include?(dupla.aluno2))
-      return true
+  def definir_primeira_dupla(aluno)
+     @todas_duplas_ordenada.each do |dupla|
+      if(dupla.aluno1.id == aluno.id || dupla.aluno2.id == aluno.id)
+        unless(lista_contem_alunos_dessa_dupla?(@duplas,dupla))
+         @duplas.push(dupla)
+       end
+      end
     end
-    false
   end
 
+   def deletar_duplas_manter_historia(alunos_manter_historia)
+    alunos_manter_historia = alunos_manter_historia.sort {|x,y| y <=> x }
+    for i in 0..(alunos_manter_historia.length-1) 
+      for j in i+1..(alunos_manter_historia.length-1) 
+        @dupla_banco = Dupla.where("aluno1_id = #{alunos_manter_historia[i].id} AND aluno2_id = #{alunos_manter_historia[j].id}").to_a[0]
+        if(@dupla_banco!=nil)
+          @todas_duplas_ordenada.delete_if { |d| d.id == @dupla_banco.id } 
+        end
+      end
+    end
+
+    for i in 0..(alunos_manter_historia.length-1) 
+      definir_primeira_dupla(alunos_manter_historia[i])
+    end
+  end
+ 
   def confirmar_dupla
     duplas = []
     unless params[:duplas] == nil
